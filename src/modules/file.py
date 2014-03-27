b95f23d6070e2bae234e1908031bce854accb0cc
Lithrein <lithrein.site@gmail.com>
2014-03-27 23:27:51 +0100

    file.py: écriture rapide de write_pieces

--- src/modules/file.py
+++ src/modules/file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+import md5
 
 class File:
     def __init__(self, abspath, waver):
@@ -16,8 +17,15 @@ class File:
             pieces = f.read(self.waver.pieces_sz)
         return pieces
 
-    def write_pieces(self, idx):
-        pass
+    def write_pieces(self, string, idx):
+        if md5.md5(string).hexdigest() == self.waver.hashes[idx - 1]:
+            with open(self.abspath + path) as f:
+                f.seek(idx_rel * self.waver.pieces_sz)
+                pieces = f.write(string)
+        else:
+            #throw Exception BadPieces
+
+
 
     def update():
         pass
